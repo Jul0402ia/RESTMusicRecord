460bad7d68490d2edc48671d0da44676018cb62b
Julia Jolanta Pawlaczyk <jup005@edu.zealand.dk>
2026-04-13 11:23:16 +0200

    MusicRecord class er blevet lavet

---
+++ RESTMusicRecord/MusicRecord.cs
@@ -0,0 +1,20 @@
+﻿namespace RESTMusicRecord
+{
+    public class MusicRecord
+    
+        {
+            public int Id { get; set; }
+            public string? Title { get; set; }
+            public string? Artist { get; set; }
+            public int Duration { get; set; }
+            public int PublicationYear { get; set; }
+
+            public override string ToString()
+            {
+                return $"Id:{Id},Title: {Title},Artist: {Artist}, Duration in seconds: {Duration}, Publication Year:{PublicationYear}";
+            }
+        }
+    }
+
+
+

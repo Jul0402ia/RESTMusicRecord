491ae2db17ed2c6b9920401dafbf7cb94d0b0b71
HelinSaraBaran <sara501d@edu.zealand.dk>
2026-04-13 12:18:43 +0200

    rettelser i program og swagger tilføjelse

--- RESTMusicRecord/Program.cs
+++ RESTMusicRecord/Program.cs
@@ -1,23 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace RESTMusicRecord
 {
-    app.MapOpenApi();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var builder = WebApplication.CreateBuilder(args);
+
+            // adds controller support 
+            builder.Services.AddControllers();
+
+            // add cors policy
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll",
+                    policy =>
+                    {
+                        policy.AllowAnyOrigin()
+                              .AllowAnyMethod()
+                              .AllowAnyHeader();
+
+
+                    });
+            });
+
+            // Add swagger services 
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+        }
+    }
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
-app.MapControllers();
 
-app.Run();

6b39f17a810f266f996623f507d68590bcd04149
Julia Jolanta Pawlaczyk <jup005@edu.zealand.dk>
2026-04-14 09:57:34 +0200

    oprettelse af dbcontext, musicrecordrepodatabase

---
+++ RESTMusicRecord/Controllers/MusicRecordController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RESTMusicRecord.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicRecordController : Controller
+    {
+        private IREPOMusicRecords _repoMusicRecords;
+
+        public MusicRecordController(IREPOMusicRecords musicRecordRepository)
+        {
+            _repoMusicRecords = musicRecordRepository;
+        }
+        
+     
+    }
+}
---
+++ RESTMusicRecord/MusicRecordDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RESTMusicRecord
+{
+    public class MusicRecordDbContext : DbContext
+    {
+        public MusicRecordDbContext(DbContextOptions<MusicRecordDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<MusicRecord> MusicRecords { get; set; }
+    }
+
+}
